e3648b05ca8488f3776ba4ede679c5be413526ed
RansonZhang <freedownzhang@vip.qq.com>
2022-06-19 17:06:53 +0800

    feat:增加对element的解析

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast';
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
 
@@ -21,8 +26,13 @@ function createRoot(children) {
 function parseChildren(context: any) {
   const nodes: any[] = [];
   let node;
-  if (context.source.startsWith('{{')) {
+  const source = context.source;
+  if (source.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if (source[0] === '<') {
+    if (/[a-z]/i.test(source[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
 
@@ -52,4 +62,23 @@ function parseInterpolation(context: any) {
 
 function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length);
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length + 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
